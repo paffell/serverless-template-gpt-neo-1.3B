6af6e25bef22c7bb70659d8d36f44927fccc0b8a
Pavel <31889570+paffell@users.noreply.github.com>
2023-03-22 22:28:31 +0100

    Update download.py

--- download.py
+++ download.py
@@ -3,19 +3,19 @@
 
 # In this example: A Huggingface GPTJ model
 
-from transformers import GPTJForCausalLM, GPT2Tokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
-    GPTJForCausalLM.from_pretrained(
-        "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True
+    AutoModelForCausalLM.from_pretrained(
+        "EleutherAI/gpt-neo-1.3B"
     )
     print("done")
 
     print("downloading tokenizer...")
-    GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
     print("done")
 
 if __name__ == "__main__":
